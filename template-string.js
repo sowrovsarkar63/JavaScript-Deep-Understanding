83f84c009a378f60eac8ea5e1a66acdbe5719fca
sowrovsarkar63 <dev.sowrovsarkar63@gmail.com>
2021-08-24 01:41:54 +0600

    template string

--- template-string.js
+++ template-string.js
@@ -1,1 +1,7 @@
-console.log("What is template string");
+const multiline = `this is first line
+this is second line 
+this is fourth line`;
+
+const name = "sowrov sarkar";
+const intro = `My name is md ${name} and I am a programmer , I have no life `;
+console.log(intro);
