359a68ec13f806a3c1e5d3926726327595e3a497
sowrovsarkar63 <dev.sowrovsarkar63@gmail.com>
2021-08-26 04:24:58 +0600

    play with distracturing

--- destructuring.js
+++ destructuring.js
@@ -1,4 +1,49 @@
 // learn about destructuring
-console.log(
-  "I think using a laptop is lot easier for me and I love to using lap than a desktop monitor"
-);
+//object destructuring
+const frontEndDev = {
+  id: 1,
+  duration: "6 month",
+  skill: [
+    "html",
+    "css",
+    "js",
+    "jquery",
+    "bootstrap",
+    "sass",
+    "coding standerd",
+    "react js ",
+    "design concept",
+    "problem solving",
+  ],
+  profession: ["job", "freelancing"],
+  position: {
+    // junior: "1.5 year",
+    mid: "2.5 year",
+    senior: "3-4 year",
+    ceo: "5-6 year",
+  },
+};
+
+const { position } = frontEndDev; //destructuring object
+const { junior = "I am already junior developer " } = position;
+
+console.log(junior);
+console.log(frontEndDev);
+
+// Array destructuring
+
+const tech = [
+  "html",
+  "css",
+  "javascript",
+  "jquery",
+  "react js ",
+  "bootstrap",
+  "git and github",
+  "coding standerd",
+  "problem solving",
+];
+
+const [foundation, stylefoundation, ...oothers] = tech;
+
+console.log(oothers[0]); //array destructuring
