12eda04cb9198aac4b4292c72c17efe61f63d22c
sowrovsarkar63 <dev.sowrovsarkar63@gmail.com>
2021-08-26 06:04:59 +0600

    find and filter also awesome features of es6

--- filter-find.js
+++ filter-find.js
@@ -4,4 +4,14 @@ const checkTenage = (age) => age < 18;
 // const GetAdult = ages.filter(checkAdult);
 const getTeen = ages.filter(checkTenage);
 
-console.log(getTeen);
+const products = [
+  { name: "water bottle", price: 50, color: "white" },
+  { name: "mobile phone", price: 15000, color: "black" },
+  { name: "head phone", price: 1200, color: "black" },
+  { name: "notes pad", price: 30, color: "blue" },
+];
+
+const Blackproducts = products.filter((product) => product.color == "black");
+for (const product of Blackproducts) {
+  console.log(product.name, product.price);
+}
